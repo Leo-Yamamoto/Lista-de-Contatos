dad476c3a24e0a6963a091b6fda0fe6af68cf6a6
Leonardo Yamamoto <leonardo0yamamoto@gmail.com>
2025-06-22 06:54:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from util.tools import *
 
-arq = 'contatos.txt'
-try:
-    open('contatos.txt').close()
-except FileNotFoundError:
-    open('contatos.txt', 'at+').close()
-    print('arquivo contatos foi criado.')
-else:
-    print(f'arquivo {arq} encontrado.')
+def mainlist():
+    arq = 'contatos.txt'
+    try:
+        open('contatos.txt').close()
+    except FileNotFoundError:
+        open('contatos.txt', 'at+').close()
+        print('arquivo contatos foi criado.')
+    else:
+        print(f'arquivo {arq} encontrado.')
 
-while True:
-    opt = menu(['Adicionar contato', 'apresentar contatos salvos', 'sair'])
-    if opt == 1:
-        titulo('NOVO CONTATO')
-        nome = str(input('Nome: '))
-        tel = str(input('Telefone: '))
-        mail = str(input('E-mail: '))
-        cadastrar(arq, nome, tel, mail)
-    elif opt == 2:
-        abrir_lista(arq)
-    elif opt == 3:
-        titulo('FINALIZADO')
-        break
+    while True:
+        opt = menu(['Adicionar contato', 'apresentar contatos salvos', 'sair'])
+        if opt == 1:
+            titulo('NOVO CONTATO')
+            nome = str(input('Nome: '))
+            tel = str(input('Telefone: '))
+            mail = str(input('E-mail: '))
+            cadastrar(arq, nome, tel, mail)
+        elif opt == 2:
+            abrir_lista(arq)
+        elif opt == 3:
+            titulo('FINALIZADO')
+            break
+if __name__ == '__main__':
+    mainlist()
